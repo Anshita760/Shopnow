c40be6c6cfacb2bdf03a436b3a0b499e7d9752c4
Anshita Verma <anshitaverma760@gmail.com>
2024-08-21 19:53:25 +0530

    product content add

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -47,18 +47,45 @@ const Product = () => {
                     <div className="product-container">
                         <div className="product-pg-detail">
                             <div className="product-nav">
-                                <NavBar />
+                                <div className="nav-comp">
+                                    <NavBar />
+                                </div>
+                                <div className="shopping-cart">
+                                    <span class="material-symbols-outlined cart">shopping_cart</span>
+                                    <div className="cart-count">
+                                        <span>10</span>
+                                    </div>
+                                </div>
                             </div>
                             <div className="product-detail-container">
                                 <div className="product-detail-content">
                                     <div className="product-full-img">
-                                        {productPanel.map((img, key) => {
+                                        {productPanel.slice(0, 1).map((img, key) => {
                                             return (
-                                                <div className=''>
-
+                                                <div className='product-img' key={key}>
+                                                    <img src={productPanel[productDetails].url} alt="" />
                                                 </div>
                                             )
                                         })}
+                                    </div>
+                                    <div className="product-full-detail">
+                                        {productPanel.slice(0, 1).map((img, key) => {
+                                            return (
+                                                <div className='product-img-detail' key={key}>
+                                                    <h2>{productPanel[productDetails].name}</h2>
+                                                    <h1>{productPanel[productDetails].price}</h1>
+                                                    <div className="divider"></div>
+                                                    <p>{productPanel[productDetails].desc}</p>
+                                                </div>
+                                            )
+                                        })}
+                                        <div className="buy-cart-btn">
+                                            <button className="buy-btn">Buy Now</button>
+                                            <button className='cart-btn'>
+                                                Go To Cart
+                                                <span class="material-symbols-outlined cart-btn-chevron">chevron_right</span>
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
